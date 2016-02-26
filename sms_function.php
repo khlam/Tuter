6e864dc34b708a166cde0ca6a529bf9eab768b9a
glen-andr <andersgl@oregonstate.edu>
2016-02-26 11:06:06 -0800

    Added files via upload

--- sms_function.php
+++ sms_function.php
@@ -3,32 +3,55 @@
 <?php include 'menu.php';?>
 <?php
  
-
+	
 	if ( isset( $_REQUEST ) && !empty( $_REQUEST ) ) {
 		 if (
  		isset( $_REQUEST['phoneNumber'], $_REQUEST['carrier']) &&
   		!empty( $_REQUEST['phoneNumber'] ) &&
-  		!empty( $_REQUEST['carrier'] )
-	 ) {
-  $custom_msg = $_REQUEST['smsMessage'];
+  		!empty( $_REQUEST['carrier'] ) &&
+	 	isset($_POST['g-recaptcha-response'])
+		) {
+  $captcha = $_POST['g-recaptcha-response'];
+  
+  $custom_msg = htmlspecialchars($_REQUEST['smsMessage'], ENT_QUOTES);
   $message = "Hello, I would like to be tutored by you (contact info here)"; 
   $to = $_REQUEST['phoneNumber'] . '@' . $_REQUEST['carrier'];
-  $check = @mail( $to, '', $message . " " . $custom_msg );
-  print 'Message was sent to ' . $to;
- } else {
-  //print 'Please fill in all fields.';
- }
-}	
+ 
+} else {
+  		print 'Please fill in all fields. ';
+   }
+	}	
+
+if($captcha){ 
+ $check = @mail( $to, '', $message . " " . $custom_msg );
+ print 'Message was sent to ' . $to;
+ } 	
+else{
+	print 'Check the captcha!';
+}
+
+
 
 ?>
 
 <!DOCTYPE html>
 
 <head>
-   <link rel="stylesheet" href="sms_function.css">
+   	<meta charset="UTF-8">
+	<link rel="stylesheet" href="sms_function.css">
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
   <body>
-   <div id="container">
+	<script type="text/javascript">
+		function ShowHide(cbox){
+			var dvSMS = document.getElementById("dvSMS");
+			
+			dvSMS.style.display = cbox.checked ? "block" : "none";   
+			
+		}
+	</script>
+
+<div id="container">
     <h1>Contact your tutor</h1>
     <form action="" method="post">
      <ul>
@@ -39,16 +62,26 @@
       <label for="carrier">Carrier (i.e. "vtext.com")</label>
        <input type="text" name="carrier" id="carrier" />
       </li>
-	  </br>
-      <li>
+	<li>
+	<label for="cbox">
+		<input type="checkbox" id="cbox" onclick="ShowHide(this)" />
+		Attach custom message
+	</label>
+	</li>
+	
+	<div id = "dvSMS" style="display: none">
+		
        <label for="smsMessage">Message</label>
        <textarea name="smsMessage" cols="40" rows="5" id="smsMessage"></textarea>
-      </li>
+      
      
-	<li><input type="submit" name="sendMessage" id="sendMessage" value="Send Message" /></li>
+	<div class="g-recaptcha" data-sitekey="6LdHHBkTAAAAAOuXXf0IO-XCMjGDE2yLx8LuHPSO" style="padding:20px"></div>
+	
+	</div>
+		
+<li><input type="submit" name="sendMessage" id="sendMessage" value="Send Message" /></li>
 </ul>
 	</form>
   </div>
  </body>
 </html>
-<?php }?>
