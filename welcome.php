ecb6648d005c5b3c4ac3d8b0906c51b5ea6bcd90
wingarlo <wingarlo@oregonstate.edu>
2016-02-27 15:36:46 -0800

    image upload
    I included image upload but I can't seem to get it to work. it's
    commented out lines 24-49

--- welcome.php
+++ welcome.php
@@ -21,7 +21,32 @@
 	set fname='$fstnm', lname='$lstnm', cstanding='$standing', phonenum='$phone'
 	where uname='$onidid'");
 	mysqli_close($conn);
+	/*
+	$image = $_POST['pic'];
+    //Stores the filename as it was on the client computer.
+    $imagename = $_FILES['pic']['name'];
+    //Stores the filetype e.g image/jpeg
+    $imagetype = $_FILES['pic']['type'];
+    //Stores any error codes from the upload.
+    $imageerror = $_FILES['pic']['error'];
+    //Stores the tempname as it is given by the host when uploaded.
+    $imagetemp = $_FILES['pic']['tmp_name'];
+
+    //The path you wish to upload the image to
+    $imagePath = 'profpics/';
 	
+    if(is_uploaded_file($_FILES['pic']['tmp_name'])) {
+        if(move_uploaded_file($imagetemp, $imagePath . $imagename)) {
+            echo "Sussecfully uploaded your image.";
+        }
+        else {
+            echo "Failed to move your image.";
+        }
+    }
+    else {
+        echo "Failed to upload your image.";
+    }
+	*/
 	include("connect.php");
 	$onidid= $_SESSION["onidid"] ;
 	$phil= $conn->query("SELECT * FROM pinfo WHERE uname='$onidid'");
