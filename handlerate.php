cedb7da376b282bd58693768c3f19e79d45b6006
sprouseamark <sprouseamark@gmail.com>
2016-03-09 21:59:57 -0800

    Lots of rating shit.

--- handlerate.php
+++ handlerate.php
@@ -36,93 +36,107 @@
 			$rating = $_POST["rate"];
 			if ($numraters == 0)
 			{
-				//just send that rate
+				//just set that rate
 				if (!$conn->query("UPDATE tuters SET rating='$rating', numraters='1' WHERE uname='$tuterID'"))
 				{
 					printf("Welp that didn't work.");
 				}
 				else
 				{
-					if (!$myfile = fopen("userfolders/$onidid/ratings.csv", "x+"))
-					{
-						$myfile = fopen("userfolders/$onidid/ratings.csv", "r+") or die("Could not open file!");
-					}
-					$alreadyExisted = 0;
-					while (!feof($myfile))
-					{
-						$currentRow = fgetcsv($myfile);
-						if($currentRow[0] == $tuterID)//if the user has already rated this user, then they already have them inside the file
-						{
-							$alreadyExisted = 1;
-							$array = array
-							(
-							"$tuterID,$rating"
-								);
-							echo "Rawr1";
-							if(!fputcsv($myfile,explode(',',$array[0])))
-								echo "The put failed1.";//might want to validate this somewhere
-								break;
-						}
-					}
-					if (!$alreadyExisted)
-					{
-						$array = array
-						(
-						"$tuterID,$rating"
-							);
-						echo "Rawr2";
-						if(!fputcsv($myfile,explode(',',$array[0])))
-							echo "The put failed2.";//might want to validate this somewhere
-					}
+					
+					$myfile = fopen("userfolders/$onidid/ratings.csv", "a") or die("Could not open file!");
+					//should come here no matter what
+					$array = array
+					(
+					"$tuterID,$rating"
+						);
+					echo "Rawr2";
+					if(!fputcsv($myfile,explode(',',$array[0])))
+						echo "The put failed2.";//might want to validate this somewhere
+				
 					fclose($myfile);
 				}
 			}
 			else
 			{
-				$newnum = $numraters+1;
-				$newRate = (($rate*$numraters)+$rating)/$newnum;
-				echo "The new rate should be $newRate";
-				if (!$conn->query("UPDATE tuters SET rating='$newRate', numraters='$newnum' WHERE uname='$tuterID'"))
+				
+				if (!$myfile = fopen("userfolders/$onidid/ratings.csv", "x+"))
 				{
-					printf("Welp that didn't work.");
+					$myfile = fopen("userfolders/$onidid/ratings.csv", "r+") or die("Could not open file!");
+				}
+				$alreadyExisted = 0;
+				$newCSV = array();			
+				while (!feof($myfile))
+				{
+
+					$currentRow = fgetcsv($myfile);
+					echo "The current row is: ";
+					print_r($currentRow);
+					echo "<BR>";
+					if($currentRow[0] == $tuterID)//if the user has already rated this user, then they already have them inside the file
+					{
+						$alreadyExisted = 1;
+						$oldrating = $currentRow[1];
+						echo "Rawr3";
+						echo "About to put into row: ";
+						print_r($currentRow);
+						echo "<BR>";
+						
+						//fputcsv($myfile,$currentRow);
+						array_push($newCSV, "$tuterID,$rating");
+						//we are now altering the average
+						echo "<br>Rate: $rate <br>";
+						echo "numraters: $numraters <br>";
+						echo "Rating: $rating <br>";
+						echo "oldrating: $oldrating <br>";
+
+						$newRate = (($rate*$numraters)+$rating-$oldrating)/$numraters;
+						echo "The new rate should be $newRate <br>";
+						if (!$conn->query("UPDATE tuters SET rating='$newRate', numraters='$numraters' WHERE uname='$tuterID'"))
+						{
+							printf("Welp that didn't work.");
+						}
+					
+					}
+					else
+						array_push($newCSV, "$currentRow[0],$currentRow[1]");
+				}
+				if (!$alreadyExisted)
+				{
+					$array = array
+					(
+					"$tuterID,$rating"
+						);
+					echo "Rawr4";
+					if(!fputcsv($myfile,explode(',',$array[0])))
+						echo "The put failed4.";//might want to validate this somewhere
+					else
+					{
+						$newnum = $numraters+1;
+						$newRate = (($rate*$numraters)+$rating)/$newnum;
+						echo "The new rate should be $newRate";
+						if (!$conn->query("UPDATE tuters SET rating='$newRate', numraters='$newnum' WHERE uname='$tuterID'"))
+						{
+							printf("Welp that didn't work.");
+						}
+					}
 				}
 				else
 				{
-					if (!$myfile = fopen("userfolders/$onidid/ratings.csv", "x+"))
-					{
-						$myfile = fopen("userfolders/$onidid/ratings.csv", "r+") or die("Could not open file!");
-					}
-					$alreadyExisted = 0;
-					while (!feof($myfile))
-					{
-						$currentRow = fgetcsv($myfile);
-						print_r($currentRow);
-						echo "<BR>";
-						if($currentRow[0] == $tuterID)//if the user has already rated this user, then they already have them inside the file
-						{
-							$alreadyExisted = 1;
-							$array = array
-							(
-							"$tuterID,$rating"
-								);
-							echo "Rawr3";
-							if(!fputcsv($myfile,explode(',',$array[0])))
-								echo "The put failed3.";//might want to validate this somewhere
-								break;
-						}
-					}
-					if (!$alreadyExisted)
-					{
-						$array = array
-						(
-						"$tuterID,$rating"
-							);
-						echo "Rawr4";
-						if(!fputcsv($myfile,explode(',',$array[0])))
-							echo "The put failed4.";//might want to validate this somewhere
-					}
+					//now rewrite the new file
 					fclose($myfile);
+					$myfile = fopen("userfolders/$onidid/ratings.csv", "w");
+					echo "Here is the new csv: <br>";
+					print_r($newCSV);
+					echo "<br>";
+					$temp = array_pop($newCSV);
+					foreach ($newCSV as $line) {
+						echo "This is the line: $line<br>";
+						if(!fputcsv($myfile, explode(',', $line)))
+							echo "RAAAAAAAAAAAAWR";
+					}
 				}
+				fclose($myfile);
 			}
 			
 	?>
